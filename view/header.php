1b4af89d5033ab14980a03c896bd8b34f3db0e77
Craig Freeburg <craigfreeburg@gmail.com>
2021-05-06 15:35:37 -0500

    few small changes, nothing of importance

--- view/header.php
+++ view/header.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
-<?php
+<!--
 // INF 653 Final Project Rest API
 // Author: Craig Freeburg
 // Date: 5/1/2021
 // File: header.php
-?>
+-->
 
 <html lang="en">
 
 <!--Head Section-->
 <head>
     <title>Quotes</title>
-    <link rel="stylesheet" type="text/css" href="./view/css/main.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Finger+Paint&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="./view/css/main.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+    <link href="https://fonts.googleapis.com/css2?family=Finger+Paint&display=swap" rel="stylesheet"/>
 </head>
 
 <!--Body Section-->
